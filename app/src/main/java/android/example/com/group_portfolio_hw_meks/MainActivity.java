d68aba3f5a91c9afe8b926ae4b0ddb68fffec08f
mlosa001 <mlosa001@gmail.com>
2018-12-02 17:13:54 -0500

    first commit

---
+++ app/src/main/java/android/example/com/group_portfolio_hw_meks/MainActivity.java
@@ -0,0 +1,13 @@
+nit package android.example.com.group_portfolio_hw_meks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
